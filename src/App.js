f9b54490fc630697f0f292d484f8681f18d049d7
qijigithub <qijilearn@gmail.com>
2023-02-01 00:06:21 -0600

    feat: rewrite canvas

--- src/App.js
+++ src/App.js
@@ -1,204 +1,111 @@
 
 import React from 'react';
 import './App.css';
+import Canvas from './Canvas';
 export const App= ()=>{
 
 
-  let color;
-  let strokeSize;
-  const [dimensions, setDimensions] = React.useState({ 
-    height: window.innerHeight,
-    width: window.innerWidth
-  })
-  const [isPortrait, setIsPortrait] =React.useState(true)
-  React.useEffect(() => {
-    function handleResize() {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth
-      })
-    
+//////////////////
+const [drawing, setDrawing] = React.useState(false)
+const [pencil, setPencil] = React.useState(0)
+const [height, setHeight] = React.useState(window.innerHeight)
+const [width, setWidth] = React.useState(window.innerWidth)
+const containerRef =React.useRef(null)
+
+const onDrawing = (context, sx, sy, cx, cy) => {
+  if(!drawing) {
+    return
+  }
+  // if(pencil === 0) {
+  //   sx *= width;
+  //   sy *= height;
+  //   cx *= width;
+  //   cy *= height;
+  //   context.beginPath();
+  //   context.moveTo(sx,sy);
+  //   context.lineTo(cx,cy);
+  //   // context.fillStyle = "black"
+  //   context.strokeStyle = 'black'
+  //   context.lineWidth = 5
+  //   context.stroke();
+  // } else 
+  if(pencil === 0) {
+    cx *= width;
+    cy *= height;
+    context.fillStyle = "white"
+    context.fillRect(cx,cy,100,100)
+  } else{
+    sx *= width;
+    sy *= height;
+    cx *= width;
+    cy *= height;
+    context.beginPath();
+    context.moveTo(sx,sy);
+    context.lineTo(cx,cy);
+    switch (pencil) {
+      case 1: context.strokeStyle = 'black'; break;
+      case 2: context.strokeStyle = 'red'; break;
+      case 3: context.strokeStyle = 'blue';break;
+      case 4: context.strokeStyle = 'green';break;
+      case 5: context.strokeStyle = 'yellow';break;
+      case 6: context.strokeStyle = 'purple';break;
+      case 7: context.strokeStyle = 'grey';break;
+    }
+    context.lineWidth = 5
+    context.stroke();
+  }
 }
-    window.addEventListener('resize', handleResize)
-  })
 
-  window.onorientationchange = (event) => {
-    const angle = event.target.screen.orientation.angle/90
-    if( angle % 2  === 0) {
-      setIsPortrait(true)
-    } else {
-      setIsPortrait(false)
-    }
-    console.log(`the orientation of the device is now ${event.target.screen.orientation.angle}, is portrait:${event.target.screen.orientation.angle/90}`);
-  };
-  // const canvasRef = React.useRef();
-
-  /// assuming canvas variable exists in global scope
-  // const PureCanvas = React.forwardRef((props, ref) => <canvas ref={ref} />);
-  function setCanvasElementSize () {
-    // Also define the gap we want at the edges:
-const edgeMargin = 10
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    const canvasAspectRatio = windowWidth / windowHeight;
-  
-    // Work out the orientation of the device.
-    // const isPortrait = window.innerHeight > window.innerWidth;
-    console.log('window width* heing', window.innerWidth, window.innerHeight, 'isPortrait' ,isPortrait)
-    if (isPortrait) {
-    
-      // We want to constrain the canvas by its width
-      ctx.canvas.width  = windowWidth - (2 * edgeMargin);
-      // The height depends on the width to ensure we don't stretch pixels
-      // on the canvas.
-      ctx.canvas.height = ctx.canvas.width / canvasAspectRatio;
-      // ctx.canvas.width =  windowWidth - (2 * edgeMargin);
-      // ctx.canvas.height = ctx.canvas.width / canvasAspectRatio;
-    } else {
-      // constrain by height
-      // canvas.style.height = windowHeight - (2 * edgeMargin);
-      // canvas.style.width = canvas.style.height * canvasAspectRatio;
-      ctx.canvas.height = windowHeight - (2 * edgeMargin);
-      ctx.canvas.width = ctx.canvas.height * canvasAspectRatio;
-    }
+const startDrawing = () =>setDrawing(true)
+const endDrawing =()=>setDrawing(false)
+const onExit = ()=> {
+  if(drawing){
+    setDrawing(false)
   }
-  
-  // Call the function once initially to size the canvas
-  
-  React.useEffect(() => {
-    // window.screen.orientation.lock('landscape');
-    // const ctx = canvasRef.current.getContext("2d");
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
-    // requestAnimationFrame(() => draw(ctx));
+}
+// React.useEffect(()=>{
+//   setDrawing(true)
+// }, [])
+const selectEraser = () => setPencil(0)
+const selectPencil = () => {
+  setPencil(1)
+}
+const selectColor = (number) => {
+  setPencil(number)
+}
 
-    const handleResize = e => {
-      ctx.canvas.height = window.innerHeight;
-      ctx.canvas.width = window.innerWidth;
+React.useEffect(()=>{
+  window.addEventListener('resize', (event)=> {
+    setWidth(window.innerWidth)
+    setHeight(window.innerHeight)
+    })
+    return ()=> {
+      window.removeEventListener('resize',()=>{})
     }
+}, [containerRef.current])
 
-    setCanvasElementSize();
-    window.addEventListener("resize",   setCanvasElementSize);
+//////////////////
 
-    return () => window.removeEventListener("resize",   setCanvasElementSize);
-    
-  }, []);
-  // const canvas = document.querySelector("#canvas");
-  // const ctx = canvas.getContext("2d");
-
-  function redraw() {
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = '5';
-    ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-  }
-
-  function resizeCanvas() {
-     const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    redraw();
-  }
-
-
-  React.useEffect(() => {
-    window.addEventListener('resize', resizeCanvas, false);
-    // Draw canvas border for the first time.
-    resizeCanvas();
-  }, [window.innerHeight, window.innerWidth]);
-  function changeColorAndSize(data, width) {
-    color = data;
-    strokeSize = width;
-  }
-  window.addEventListener("load", () => {
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
-    //resizing
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    // ctx.canvas.width =window.innerWidth;;
-    // ctx.canvas.height = window.innerHeight;
-    // setCanvasElementSize();
-    // handleResize(e)
-    redraw()
-    //variables
-    let painting = false;
-  
-    //functions
-    function startPosition(e) {
-      painting = true;
-      draw(e);
-    }
-  
-    function endPosition() {
-      painting = false;
-      ctx.beginPath();
-    }
-  
-    function draw(e) {
-      if (!painting) {
-        return;
-      }
-      
-      e.preventDefault();
-      ctx.lineWidth = strokeSize;
-      ctx.lineCap = "round";
-   
-      // ctx.lineTo(e.clientX, e.clientY);
-      if (e.type === 'touchmove'){
-        ctx.lineTo(e.touches[0].clientX, e.touches[0].clientY);
-      } else if (e.type === 'mousemove'){
-        ctx.lineTo(e.clientX, e.clientY);
-      }
-        
-      ctx.stroke();
-      ctx.strokeStyle = color;
-     ctx.lineWidth = strokeSize;
-      ctx.beginPath();
-      
-      // ctx.moveTo(e.clientX, e.clientY);
-      if (e.type === 'touchmove'){
-        ctx.moveTo(e.touches[0].clientX, e.touches[0].clientY);
-      } else if (e.type === 'mousemove'){
-        ctx.moveTo(e.clientX, e.clientY);
-      }
-    }
-  
-    //event listeners
-    canvas.addEventListener("mousedown", startPosition);
-    canvas.addEventListener("touchstart", startPosition);
-    canvas.addEventListener("mouseup", endPosition);
-    canvas.addEventListener("touchend", endPosition);
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("touchmove", draw);
-  });
-
-  return (
-    <div className="App">
-
-        <div id="colorButtonBox">
-    <div id="colorButton" className="black" onClick={()=>changeColorAndSize('black',5)}></div>
-    <div id="colorButton" className="red" onClick={()=>changeColorAndSize('red',5)}></div>
-    <div id="colorButton" className="green" onClick={()=>changeColorAndSize('green',5)}></div>
-    <div id="colorButton" className="blue" onClick={()=>changeColorAndSize('blue',5)}></div>
-    <div id="colorButton" className="yellow" onClick={()=>changeColorAndSize('yellow',5)}></div>
-    <div id="colorButton" className="purple" onClick={()=>changeColorAndSize('purple',5)}></div>
-    <div id="colorButton" className="grey" onClick={()=>changeColorAndSize('grey',5)}></div>
-    <div id="eraserButton" onClick={()=>changeColorAndSize('white',100)}></div>
+return (
+  <>
+  <div ref={containerRef}>
+  <Canvas onDraw={onDrawing} onStart={startDrawing} onStop={endDrawing} onEnd={onExit} height={height} width={width}/>
   </div>
-  {/* <PureCanvas id="canvas" ref={canvasRef} /> */}
-  {/* <canvas id="canvas"></canvas> */}
-  <h1>{`window width* height', ${window.innerWidth}, ${window.innerHeight}`}</h1>
-  <h1>{`'isPortrait' ,${isPortrait}`}</h1>
-  <div>Rendered at {dimensions.width} x {dimensions.height}</div>
-  <canvas id='canvas' style={{position:'fixed', left:'0px', top:'0px'}}></canvas>
-    </div>
-  );
+  <div id="colorButtonBox">
+  <div id="colorButton" className="black" onClick={selectPencil}></div>
+  <div id="colorButton" className="red" onClick={()=>selectColor(2)}></div>
+    <div id="colorButton" className="blue" onClick={()=>selectColor(3)}></div>
+    <div id="colorButton" className="green" onClick={()=>selectColor(4)}></div>
+    <div id="colorButton" className="yellow" onClick={()=>selectColor(5)}></div>
+    <div id="colorButton" className="purple" onClick={()=>selectColor(6)}></div>
+    <div id="colorButton" className="grey" onClick={()=>selectColor(7)}></div>
+    <div id="eraserButton" className="white" onClick={selectEraser}></div>
+    {/* <button onClick={selectPencil}>pencil</button>
+    <button onClick={selectEraser}>eraser</button> */}
+  </div>
+  </>
+)
+
 
 }
 
---
+++ src/Canvas.js
@@ -0,0 +1,79 @@
+import React from 'react'
+// function Point(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+const Canvas = ({onStart,onDraw, onEnd, onStop,height, width, className }) => {
+const canvasRef = React.useRef(null)
+const [start, setStart] = React.useState({x:0,y:0})
+// const { Height, Width } = canvasStore;
+React.useEffect(()=>{
+const canvas = canvasRef.current;
+if(!canvas) {
+    return
+}
+canvas.height = height;
+canvas.width = width;
+canvas.style.width = "100%"
+canvas.style.height = "100%"
+}, [width,height])
+
+const startDrawing = ({nativeEvent}) => {
+    let {offsetX, offsetY} = nativeEvent
+    const canvas = canvasRef.current
+    if(!canvas) {
+        return
+    }
+    if(window.TouchEvent){
+        if(nativeEvent.changedTouches?.length){
+          offsetX = nativeEvent.changedTouches[0].clientX - canvas.offsetLeft;
+          offsetY = nativeEvent.changedTouches[0].clientY - canvas.offsetTop;
+        }
+      }
+    const bound = canvas.getBoundingClientRect()
+    const normalizeX = offsetX/bound.width
+    const normalizeY = offsetY/bound.height
+    setStart({x:normalizeX,y:normalizeY})
+    onStart()
+}
+
+const finishDrawing = () => {
+    onEnd()
+}
+const draw = ({nativeEvent}) => {
+    let {offsetX, offsetY} = nativeEvent
+    const canvas = canvasRef.current
+    if(!canvas) {
+        return
+    }
+    if(window.TouchEvent){
+        if(nativeEvent.changedTouches?.length){
+          offsetX = nativeEvent.changedTouches[0].clientX - canvas.offsetLeft;
+          offsetY = nativeEvent.changedTouches[0].clientY - canvas.offsetTop;
+        }
+      }
+    const context = canvas.getContext('2d')
+    const bound = canvas.getBoundingClientRect()
+    const normalizeX = offsetX/bound.width
+    const normalizeY = offsetY/bound.height
+    onDraw(context,start.x,start.y,normalizeX, normalizeY)
+    setStart({x:normalizeX,y:normalizeY})
+}
+const canvasLeave = () => {
+    onStop()
+}
+  return (
+    <canvas 
+    ref={canvasRef} 
+    onMouseDown={startDrawing} 
+    onMouseMove={draw} 
+    onMouseUp={finishDrawing} 
+    onMouseLeave={canvasLeave}     
+    onTouchStart={startDrawing}
+    onTouchEnd={finishDrawing}
+    onTouchMove={draw}
+    className={className} style={{border:'2px solid red'}}></canvas>
+  )
+}
+
+export default Canvas
